aa282b52c0c698adf10a5841687d88d159fd5a82
Breno <brenoss.magalhaes@outlook.com>
2022-05-03 12:06:32 -0300

    aprendendo oop em python

--- challages/94.py
+++ challages/94.py
@@ -1,26 +1,27 @@
+from typing import List
+
+
 cnpj = "04.252.011/0001-10"
 
 dig1_validador = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 dig2_validador = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
 
+def calcDig(cnpj: List, validador):
+    dig = 11 - sum([x * y for x, y in zip(cnpj, validador)]) % 11
+    if dig > 9:
+        dig = 0
+    cnpj.append(dig)
+    return cnpj
+
+
 cnpj = cnpj.replace(".", "").replace("/", "").replace("-", "")
 
 listaPrimeiro = [int(x) for x in list(cnpj[:12])]
-print(listaPrimeiro)
-print(dig1_validador)
 
-digito1 = 11 - sum([x * y for x, y in zip(listaPrimeiro, dig1_validador)]) % 11
-if digito1 > 9:
-    digito1 = 0
+listaPrimeiro = calcDig(cnpj=listaPrimeiro, validador=dig1_validador)
+listaPrimeiro = calcDig(listaPrimeiro, dig2_validador)
 
-listaPrimeiro.append(digito1)
-
-digito2 = 11 - sum([x * y for x, y in zip(listaPrimeiro, dig2_validador)]) % 11
-if digito2 > 9:
-    digito2 = 0
-
-listaPrimeiro.append(digito2)
 
 if cnpj == "".join(str(x) for x in listaPrimeiro):
     print("CNPJ válido")
--- poo/classes.py
+++ poo/classes.py
@@ -1,3 +1,49 @@
+from datetime import datetime
+from random import randint
+
+
 class Pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), "%Y"))
+
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    def idade(self):
+        return self.ano_atual - self.idade
+
     def falar(self):
-        print("Pessoa está falando...")
+        print(self.nome)
+
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)
+
+    @staticmethod
+    def gera_id():
+        rand = randint(10000, 19999)
+        return rand
+
+
+print(Pessoa.gera_id())
+
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def desconto(self, porcentagem):
+        return self.preco - (self.preco * (porcentagem / 100))
+
+
+p1 = Produto("Notebook", 2999.99)
+p1.desconto(10)
+print(p1.preco)
+
+p2 = Produto("Notebook", 2999.99)
+p2.desconto(20)
+print(p2.preco)
